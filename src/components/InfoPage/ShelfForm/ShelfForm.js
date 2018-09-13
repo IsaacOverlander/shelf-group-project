c58aad6b75474459f9e64c4ff19060d0b0c18ea1
Isaac Overlander <Isaac.overlander@gmail.com>
2018-09-13 14:06:13 -0500

    creataed funcitons for form

--- src/components/InfoPage/ShelfForm/ShelfForm.js
+++ src/components/InfoPage/ShelfForm/ShelfForm.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 
 class ShelfForm extends Component {
     // ** FUNCTIONS ** 
+    constructor() {
+        super();
+        this.state = {
+            description: '',
+            image_url: '',
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            [event.target.name]: evemt.target.value,
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const action = {type: 'SET_SHELF', payload: this.state}
+        console.log(action);
+    };
 
     // ** FUNCTIONS **
     render() {

ef96dd69f952ae3ea8c8d6695d67c07be0245781
spitz-the-great <jspitzley85@gmail.com>
2018-09-13 16:51:52 -0500

    fixed comments

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -46,55 +46,7 @@ class InfoPage extends Component {
   getShelf() {
 
     this.props.dispatch({ type: 'FETCH_SHELF' })
-    // axios({
-    //   method: 'GET',
-    //   url: '/api/shelf'
-    // }).then((response) => {
-    //   const action = { type: 'SET_SHELF', payload: response.data }
-    //   this.props.dispatch(action)
-
-    // });
   }
-
-//   function* rootSaga() {
-//     // step 8 takeEvery will listen for specific actions
-//     // when we get an action of type 'FETCH_BASKET', call getBasketSaga
-//     // first param of takeEvery takes an action.type
-//     yield takeEvery('FETCH_BASKET', getBasketSaga);
-//     // when we dispatch 'ADD_BASKET_ITEM' , call postBasketSaga
-//     yield takeEvery('ADD_BASKET_ITEM', postBasketSaga);
-
-// }
-//  // add basket item and then call FETCH_BASKET
-// function* postBasketSaga(action) {
-//     try {
-//         // post to /fruit with data of action.payload
-//         yield call(axios.post, '/fruit', action.payload);
-//         // put dispatches an action
-//         yield put({ type: 'FETCH_BASKET' });
-//     } catch (error) {
-//         console.log(error);
-//         alert('unable to add item');
-//     }
-// }
-
-// // step 6 create individual sagas
-// function* getBasketSaga(action) {
-//     // similar to reducers sagas will get an action
-//     console.log('get basket saga with action: ', action);
-//     try {
-//         // step 12 making http req to server
-//         const basketResponse = yield call(axios.get, '/fruit');
-//         // basketResponse is our response from server
-//         // step 13 dispatch an action and send to redux
-//         const responseAction = { type: 'SET_BASKET', payload: basketResponse.data };
-//         yield put(responseAction);
-//     } catch (error) {
-//         // notify user something went wrong
-//         alert('unable to get basket');
-//         console.log(error);
-//     }
-// }
 
   render() {
     let content = null;
